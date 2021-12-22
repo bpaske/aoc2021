4b1de208e35231c2d6b444a58888a3f8bdc23947
Ben Paske <brpaske@googlemail.com>
2021-12-22 21:22:47 +0000

    optimisation for day 21

--- 21/solution2-recursive.py
+++ 21/solution2-recursive.py
@@ -4,11 +4,9 @@ from functools import cache
 roll_frequencies = Counter(a+b+c for a in range(1,4) for b in range(1,4) for c in range(1,4))
 
 @cache
-def calculate_wins(game_state, player1_turn):
+def calculate_wins(game_state):
     (player1_position, player1_score, player2_position, player2_score) = game_state
 
-    if player1_score >= 21:
-        return 1, 0
     if player2_score >= 21:
         return 0, 1
 
@@ -16,19 +14,14 @@ def calculate_wins(game_state, player1_turn):
     player2_wins = 0
 
     for roll, roll_count in roll_frequencies.items():
-        if(player1_turn):
-            new_position = (player1_position + roll) % 10
-            new_score = ( player1_score + new_position ) if new_position !=0 else (player1_score + 10)
-            next_state = (new_position, new_score, player2_position, player2_score)
-        else:
-            new_position = (player2_position + roll) % 10
-            new_score = ( player2_score + new_position ) if new_position !=0 else (player2_score + 10)
-            next_state = (player1_position, player1_score, new_position, new_score)
+        new_position = (player1_position + roll) % 10
+        new_score = ( player1_score + new_position ) if new_position !=0 else (player1_score + 10)
+        next_state = ( player2_position, player2_score, new_position, new_score)
 
-        additional_player1_wins, additional_player2_wins= calculate_wins(next_state, not player1_turn)
+        additional_player2_wins, additional_player1_wins = calculate_wins(next_state)
         player1_wins += roll_count*additional_player1_wins
         player2_wins += roll_count*additional_player2_wins
 
     return player1_wins, player2_wins
 
-print(calculate_wins((4,0,8,0), True))
+print(calculate_wins((4,0,8,0)))
